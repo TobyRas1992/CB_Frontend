0773a9cff41f86d30de5b21b36d17dc46141e58f
Tobias Rasmussen <tobiasrasmussen@outlook.com>
2022-02-22 10:33:08 +0100

    start of IIFE + loading message added to document upon start

--- script.js
+++ script.js
@@ -1,12 +1,34 @@
-let userList = [];
-let apiURL = "https://jsonplaceholder.typicode.com/users";
-
 /* let getUsers = () => {
   fetch(apiURL)
     .then((response) => response.json())
     .then((json) => console.log(json));
 }; */
-
+/* 
 fetch(apiURL)
   .then((response) => response.json())
   .then((json) => console.log(json));
+ */
+let userListing = (function () {
+  let userList = [];
+  let apiURL = "https://jsonplaceholder.typicode.com/users";
+
+  let showLoadingMessage = () => {
+    let loadingMessage = document.createElement("P");
+    loadingMessage.innerText = "Loading. One moment!";
+    document.body.appendChild(loadingMessage);
+  };
+
+  let hideLoadingMessage = () => {
+    let elementToRemove = document.querySelector("P");
+    elementToRemove.parentElement.removeChild(elementToRemove);
+  };
+
+  let loadList = () => {
+    showLoadingMessage();
+  };
+  return {
+    loadList: loadList,
+  };
+})();
+
+userListing.loadList();
