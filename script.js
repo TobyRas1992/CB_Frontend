8efaa730a09fd50b9f5c282a60f1baaed4b2dea2
Tobias Rasmussen <tobiasrasmussen@outlook.com>
2022-02-22 10:18:37 +0100

    fetch success = users are retrived from API

--- script.js
+++ script.js
@@ -1,2 +1,12 @@
-//variable for holding users
 let userList = [];
+let apiURL = "https://jsonplaceholder.typicode.com/users";
+
+/* let getUsers = () => {
+  fetch(apiURL)
+    .then((response) => response.json())
+    .then((json) => console.log(json));
+}; */
+
+fetch(apiURL)
+  .then((response) => response.json())
+  .then((json) => console.log(json));
